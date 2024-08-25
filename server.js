a0195860ec12513e89a8ca3a370dd9892497af19
rituraj1314 <riturajsandy1314@gmail.com>
2024-08-25 15:38:04 +0530

    New

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: ['https://bajaj-fh-challege.vercel.app', 'http://localhost:3000']
+  }));
 app.use(express.json());
 
 // POST endpoint
